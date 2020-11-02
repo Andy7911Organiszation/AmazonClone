2c205c84af84734e6a90252125fcca08e48e3d43
andersonbeauvais <e1061615@cmaisonneuve.qc.ca>
2020-11-02 17:57:49 -0500

    ajout de token authentification et de cookie

--- src/component/DetailPageProduct.js
+++ src/component/DetailPageProduct.js
@@ -25,100 +25,104 @@ export default class DetailPageProduct extends Component {
         return (
             <div className='container-fluid'>
                 <Nav></Nav>
-                <div className ='columns'>
-                    <div className='column is-1 '>
-                        <img src="https://i.ibb.co/L5Lp1Tx/aspirateur.png" className='is-gray-border' style={{width:100,height:100, }} alt='imagefacade' onMouseOver={event=>this.overImage(event)} ></img> 
-                 
-                    </div>
-                    <div className='column is-1'>
-                        <img src="https://i.ibb.co/hWcs30T/robot-Culinaire.png" className='is-gray-border'  style={{width:100,height:100}} alt='imagefacade' onMouseOver={event=>this.overImage(event)}></img> 
-                    </div>
-                    <div className='column is-1' >
-                 
-                        <img src="https://i.ibb.co/92GYz9h/bbq.png"  className='is-gray-border' style={{width:100,height:100}} alt='imagefacade' onMouseOver={event=>this.overImage(event)}></img>
-                    </div>
-             
- 
-                </div>
-                <div className='columns is-3'>
-                    <div className='column'>
-                        <img src={this.state.file} style={{width:'400px',height:'400px'}}> 
-
-                        </img>
-                    </div>
-                    <div className='column is-5'>
-                        <h1 className='title is-3'>BBq Grill</h1>
-                        <hr></hr>
-                        <p>
-                            They are present from the beginning of the development of the cell-wail, and arise from the spindle 
-                            fibres, all of which may be continued as connecting threads (endosperm of Tamus communis), or part of 
-                            them may be overlaid by cellulose lamellae (endosperm of Lilium Martagon),
-                            or they may be all overlaid as in pollen mother-cells and pollen grains of Helleborus foetidus.
-                        </p>
-
-                    </div>
-                    <div className='column is-2 pl-2'>
-                        <div >
-                        <div className='columns is-desktop is-vcentered'>
-                        <div className='column is-12 p-4'>
-                        
-                            <div className='card has-text-centered is-wide' >
-                             <div className=''>
-                             <p className='has-text-weight-semibold pb-6'>Souhaitez-vous protéger votre achat? Vérifiez que cette assurance couvre vos besoins:</p>
-                              <p >1800$ et livraison gratruite</p> 
-                              <p className="pb-4">Seulement 10 en stock</p> 
-                              <p className='has-text-primary'>{this.state.stock}</p>
-                              Quantité:
-                                    <div class="select is-small">
-                                        <select>
-                                            <option></option>
-                                            <option></option>
-                                        </select>
-                                    </div>
-                                  
-                                   <button className='button gold is-normal is-outlined mb-2 mt-4' style={{width:'80%'}}><FontAwesomeIcon transform="grow-7 left-30.5 up-2.2" icon={faCartPlus}/> Ajouter au Panier</button>
-                                   <button className='button gold is-normal is-fullwidth pl-3' ><FontAwesomeIcon  icon={faPlay}/>Acheter Maintenant</button>
-                                   <p><FontAwesomeIcon  icon={faLock}/> &nbsp; Trasaction sécuritaire </p>
-                                   <hr className='black'/>
-                                   <p> <FontAwesomeIcon icon={faMapMarker}/> &nbsp; Livre a Beaudois Montreal Qc </p>
-                                   <button className='buttton is-light is-small'> Ajouter a la liste d'envie</button>
-                            </div>
-
-                            </div>
-
-                          
-                            <div>
-
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                        {/*ending Block*/}
-                        <div className='pt-4'> 
-                        
-                         <div className='card'>
-                                <div className='card-header-title is-centered'>
-                                    <p >Autre Vendeur sur Amazon </p>
-                                </div>
-                                <hr className='black'></hr>
-                                <div className='card-content'>
-                                    <div className='has-text-right'>
-                                         35,77$ <button className='button gold is-small' >Ajouter au panier</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-                    <div className='column'></div>
-
+                <div className='container'>
+                    <div className ='columns'>
+                        <div className='column is-1 '>
+                            <img src="https://i.ibb.co/L5Lp1Tx/aspirateur.png" className='is-gray-border' style={{width:100,height:100, }} alt='imagefacade' onMouseOver={event=>this.overImage(event)} ></img> 
                     
+                        </div>
+                        <div className='column is-1'>
+                            <img src="https://i.ibb.co/hWcs30T/robot-Culinaire.png" className='is-gray-border'  style={{width:100,height:100}} alt='imagefacade' onMouseOver={event=>this.overImage(event)}></img> 
+                        </div>
+                        <div className='column is-1' >
+                    
+                            <img src="https://i.ibb.co/92GYz9h/bbq.png"  className='is-gray-border' style={{width:100,height:100}} alt='imagefacade' onMouseOver={event=>this.overImage(event)}></img>
+                        </div>
+                
+    
+                    </div>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <img src={this.state.file} style={{width:'400px',height:'400px'}}> 
 
-                </div>
+                            </img>
+                        </div>
+                        <div className='column is-5'>
+                            <h1 className='title is-3'>BBq Grill</h1>
+                            <hr></hr>
+                            <p>
+                                They are present from the beginning of the development of the cell-wail, and arise from the spindle 
+                                fibres, all of which may be continued as connecting threads (endosperm of Tamus communis), or part of 
+                                them may be overlaid by cellulose lamellae (endosperm of Lilium Martagon),
+                                or they may be all overlaid as in pollen mother-cells and pollen grains of Helleborus foetidus.
+                            </p>
+
+                        </div>
+                      
+                            <div >
+                           
+                            <div className='column is-8 p-4 '>
+                            
+                                <div className='card has-text-centered is-wide' >
+                                <div className=''>
+                                <p className='has-text-weight-semibold pb-6'>Souhaitez-vous protéger votre achat? Vérifiez que cette assurance couvre vos besoins:</p>
+                                <p >1800$ et livraison gratruite</p> 
+                                <p className="pb-4">Seulement 10 en stock</p> 
+                                <p className='has-text-primary'>{this.state.stock}</p>
+                                Quantité:
+                                        <div class="select is-small">
+                                            <select>
+                                                <option></option>
+                                                <option></option>
+                                            </select>
+                                        </div>
+                                    
+                                    <button className='button gold is-normal is-outlined mb-2 mt-4' style={{width:'80%'}}><FontAwesomeIcon transform="grow-7 left-30.5 up-2.2" icon={faCartPlus}/> Ajouter au Panier</button>
+                                    <button className='button gold is-normal is-fullwidth pl-3' ><FontAwesomeIcon  icon={faPlay}/>Acheter Maintenant</button>
+                                    <p><FontAwesomeIcon  icon={faLock}/> &nbsp; Trasaction sécuritaire </p>
+                                    <hr className='black'/>
+                                    <p> <FontAwesomeIcon icon={faMapMarker}/> &nbsp; Livre a Beaudois Montreal Qc </p>
+                                    <button className='buttton is-light is-small'> Ajouter a la liste d'envie</button>
+                                </div>
+
+                                </div>
+
+                            
+                                <div>
+
+                                </div>
+
+                                    <div className='card'>
+                                    <div className='card-header-title is-centered'>
+                                        <p >Autre Vendeur sur Amazon </p>
+                                    </div>
+                                    <hr className='black'></hr>
+                                    <div className='card-content'>
+                                        <div className='has-text-right'>
+                                            <h2>35,77$</h2> <button className='button gold is-small' >Ajouter au panier</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                </div>
+                             
+                            
+
+                            </div>
+                            </div>
+                            {/*ending Block*/}
+                            <div className='pt-4'> 
+                          
+
+                     
+                        <div className='column'></div>
+
+                        
+
+                    </div>
                 <Footer></Footer>
-
-        </div>
+                </div>
+            </div>
         )
     }
 }
